236f4891a63ace013582106e548c4440ca3e3b6d
Perlashaddad <perla.s.haddad@gmail.com>
2026-06-02 00:18:15 +0400

    Updates

---
+++ _next/static/p9ok9l1CMRM3oxsHJV7iX/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/grabme-product-system//_next/:path+",
+        "destination": "/grabme-product-system/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
